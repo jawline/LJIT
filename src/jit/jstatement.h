b21d67bee18b8b68f98678716d515edaa2e44a4d
Blake Loring <blake_l@parsed.uk>
2015-05-27 10:28:26 +0100

    Create jstatement.h

---
+++ src/jit/jstatement.h
@@ -0,0 +1,14 @@
+#ifndef _JSTATEMENT_DEF_H_
+#define _JSTATEMENT_DEF_H_
+
+namespace JIT {
+  class Statement {
+  private:
+    int64_t val;
+    bool add;
+
+  public:
+  };
+}
+
+#endif //_JSTATEMENT_DEF_H_
