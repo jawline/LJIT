9f4cc90dcb5ab89996bd3630bf132493cb90b272
Blake Loring <blake_l@parsed.uk>
2015-05-27 13:15:40 +0100

    Update jstatement.h

--- src/jit/jstatement.h
+++ src/jit/jstatement.h
@@ -4,15 +4,23 @@
 #include "../utils/bytebuffer.h"
 
 namespace JIT {
-    
+
+    enum StatementType {
+        Atom,
+        Add,
+        Subtract,
+        Multiply,
+        Divide
+    }
+
     class Statement {
         private:
+          StatementType _type;
           int64_t _val;
-          bool _add;
           SafeStatement _lhs, _rhs;
         public:
           Statement(int64_t val);
-          Statement(SafeStatement lhs, SafeStatement rhs);
+          Statement(StatementType type, SafeStatement lhs, SafeStatement rhs);
           void write(ByteBuffer& buffer);
     };
     
