3f434151b6e21d59d7eeec7fd8ba98b0386542c2
Blake Loring <blake_l@parsed.uk>
2015-05-27 10:44:20 +0100

    Update jfunction.h

--- src/jit/jfunction.h
+++ src/jit/jfunction.h
@@ -2,11 +2,14 @@
 #define _JIT_FUNCTION_DEF_H_
 #include "../utils/bytebuffer.h"
 #include "jstatement.h"
+#include "jhelper.h"
 
 namespace JIT {
 	class JFunction {
 	private:
 		void prepare(JStatement  const& stmt);
+		JFPTR _storedFn;
+		size_t _fnSize;
 
 	public:
 		JFunction(JStatement stmt);
