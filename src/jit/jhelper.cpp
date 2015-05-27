8cdc4e2a05e46895b992975b04b19846d23601ce
Blake Loring <blake_l@parsed.uk>
2015-05-27 10:21:19 +0100

    Create jhelper.cpp

---
+++ src/jit/jhelper.cpp
@@ -0,0 +1,15 @@
+#include "jhelper.h"
+
+using namespace JIT;
+
+void Helper::insertPrologue(ByteBuffer& buffer) {
+}
+
+void Helper::insertEpilogue(ByteBuffer& buffer) {
+}
+
+void Helper::pushNumber(int64_t value, ByteBuffer& buffer) {
+}
+
+void Helper::addTopTwoStack(ByteBuffer& buffer) {
+}
