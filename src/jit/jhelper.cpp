eccfd05508848d53414e85831dccc8af9c850e8f
Blake Loring <blake_l@parsed.uk>
2015-05-27 10:26:19 +0100

    Update jhelper.cpp

--- src/jit/jhelper.cpp
+++ src/jit/jhelper.cpp
@@ -4,26 +4,26 @@
 using namespace JIT;
 
 void Helper::insertPrologue(ByteBuffer& buffer) {
-  buffer.insert(Template::prologue, sizeof(Template::prologue));
+    buffer.insert(Template::prologue, sizeof(Template::prologue));
 }
 
 void Helper::insertEpilogue(ByteBuffer& buffer) {
-  buffer.insert(Template::epilogue, sizeof(Template::epilogue));
+    buffer.insert(Template::epilogue, sizeof(Template::epilogue));
 }
 
 void Helper::pushNumber(int64_t value, ByteBuffer& buffer) {
-  //mov _val, RAX
-	uint8_t mrax[2] = { 0x48, 0xB8 };
-	buffer.insert(mrax, 2);
-	buffer.insert((int64_t) _val);
-	
-	//push RAX
-	buffer.insert((uint8_t)0x50);
+    //mov _val, RAX
+    uint8_t mrax[2] = { 0x48, 0xB8 };
+    buffer.insert(mrax, 2);
+    buffer.insert((int64_t) _val);
+    
+    //push RAX
+    buffer.insert((uint8_t)0x50);
 }
 
 void Helper::addTopTwoStack(ByteBuffer& buffer) {
-  //pop RCX
-  //pop RAX
-  //add RCX, RAX
-  //push RAX
+    //pop RCX
+    //pop RAX
+    //add RCX, RAX
+    //push RAX
 }
