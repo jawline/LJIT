e7cbfa4b3465f0e5c6c5031985fc68b3a991cfe9
Blake Loring <blake_l@parsed.uk>
2015-05-27 13:19:32 +0100

    Update jstatement.cpp

--- src/jit/jstatement.cpp
+++ src/jit/jstatement.cpp
@@ -4,19 +4,26 @@
 using namespace JIT;
 
 Statement::Statement(int64_t val) {
+  _type = Atom;
   _val = val;
 }
 
-Statement::Statement(SafeStatement lhs, SafeStatement rhs) {
+Statement::Statement(StatementType type, SafeStatement lhs, SafeStatement rhs) {
+  _type = type;
   _lhs = lhs;
   _rhs = rhs;
-  _add = true;
 }
 
 void Statement::write(ByteBuffer& buffer) {
-  if (_add) {
-    Helper::addTopTwoStack(buffer);
-  } else {
-    Helper::pushNumber(_val);
+  switch (_type) {
+    case Atom:
+      Helper::addTopTwoStack(buffer);
+      break;
+    case Add:
+      Helper::addTopTwoStack(buffer);
+      break;
+    default:
+      printf("Could not JIT\n");
+      break;
   }
 }
