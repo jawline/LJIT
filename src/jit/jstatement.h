9d5678ebf86371fcf50b7416851b1ac85e920c1f
Blake Loring <blake_l@parsed.uk>
2015-05-27 13:06:45 +0100

    Update jstatement.h

--- src/jit/jstatement.h
+++ src/jit/jstatement.h
@@ -1,16 +1,22 @@
 #ifndef _JSTATEMENT_DEF_H_
 #define _JSTATEMENT_DEF_H_
+#include <memory>
 #include "../utils/bytebuffer.h"
 
 namespace JIT {
-  class Statement {
-  private:
-    int64_t _val;
-    bool _add;
-  public:
-    Statement(int64_t val);
-    void write(ByteBuffer& buffer);
-  };
+    
+    class Statement {
+        private:
+          int64_t _val;
+          bool _add;
+          SafeStatement _lhs, _rhs;
+        public:
+          Statement(int64_t val);
+          Statement(SafeStatement lhs, SafeStatement rhs);
+          void write(ByteBuffer& buffer);
+    };
+    
+    typedef std::shared_ptr<Statement> SafeStatement;
 }
 
 #endif //_JSTATEMENT_DEF_H_
