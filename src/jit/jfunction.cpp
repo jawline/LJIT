08d0d591ee38ef15524c413ee85ab61ae1b19b90
Blake Loring <blake_l@parsed.uk>
2015-05-27 10:30:18 +0100

    Update jfunction.cpp

--- src/jit/jfunction.cpp
+++ src/jit/jfunction.cpp
@@ -2,18 +2,5 @@
 
 using namespace JIT;
 
-JFunction::JFunction(int64_t val) {
-	_val = val;
-}
+JFunction::JFunction(Statement stmt) {}
 JFunction::~JFunction() {}
-
-void JFunction::write(Assembler::ByteBuffer& buffer) {
-
-	//mov RAX, _val
-	uint8_t mrax[2] = { 0x48, 0xB8 };
-	buffer.insert(mrax, 2);
-	buffer.insert((int64_t) _val);
-	
-	//push RAX
-	buffer.insert((uint8_t)0x50);
-}
