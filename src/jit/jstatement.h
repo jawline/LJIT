afb7682c8bd355d121eedfdd3864829f5c1c19d7
Blake Loring <blake_l@parsed.uk>
2015-05-27 10:29:56 +0100

    Update jstatement.h

--- src/jit/jstatement.h
+++ src/jit/jstatement.h
@@ -1,13 +1,15 @@
 #ifndef _JSTATEMENT_DEF_H_
 #define _JSTATEMENT_DEF_H_
+#include "../utils/bytebuffer.h"
 
 namespace JIT {
   class Statement {
   private:
     int64_t val;
     bool add;
-
   public:
+    Statement(int64_t val);
+    void write(ByteBuffer& buffer);
   };
 }
 
