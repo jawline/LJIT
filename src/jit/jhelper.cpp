5bb316e69f0d1885fa1d2d719b69d1379789a527
Blake Loring <blake_l@parsed.uk>
2015-05-27 10:22:48 +0100

    Update jhelper.cpp

--- src/jit/jhelper.cpp
+++ src/jit/jhelper.cpp
@@ -1,11 +1,14 @@
 #include "jhelper.h"
+#include "jtemplate.h"
 
 using namespace JIT;
 
 void Helper::insertPrologue(ByteBuffer& buffer) {
+  buffer.insert(Template::prologue, sizeof(Template::prologue));
 }
 
 void Helper::insertEpilogue(ByteBuffer& buffer) {
+  buffer.insert(Template::epilogue, sizeof(Template::epilogue));
 }
 
 void Helper::pushNumber(int64_t value, ByteBuffer& buffer) {
