42f02c89070ef4666ec94f8f9aa6cea2e895abe6
Blake Loring <blake_l@parsed.uk>
2015-05-27 10:34:53 +0100

    Update jfunction.cpp

--- src/jit/jfunction.cpp
+++ src/jit/jfunction.cpp
@@ -1,9 +1,15 @@
 #include "jfunction.h"
+#include "jhelper.h"
 
 using namespace JIT;
 
 JFunction::JFunction(Statement stmt) {}
 JFunction::~JFunction() {}
 
-void JFunction::prepare(ByteBuffer const& buffer) {
+void JFunction::prepare(Statement const& stmt) {
+  ByteBuffer buffer;
+
+  Helper::insertPrologue(buffer);
+  stmt.write(buffer);
+  Helper::insertEpilogue(buffer);
 }
