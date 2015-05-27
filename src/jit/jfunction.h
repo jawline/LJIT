610b5923b4e3fe5c9c3431fe233937757f9b8ae4
Blake Loring <blake_l@parsed.uk>
2015-05-27 10:29:07 +0100

    Update jfunction.h

--- src/jit/jfunction.h
+++ src/jit/jfunction.h
@@ -1,21 +1,14 @@
 #ifndef _JIT_FUNCTION_DEF_H_
 #define _JIT_FUNCTION_DEF_H_
 #include "../utils/bytebuffer.h"
-#include <string>
-#include <vector>
-using namespace std;
 
 namespace JIT {
 
 class JFunction {
 private:
-	int64_t _val;
-
 public:
-	JFunction(int64_t x);
+	JFunction(JStatement stmt);
 	~JFunction();
-
-	void write(Assembler::ByteBuffer& buffer);
 };
 
 }
