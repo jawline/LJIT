38289989dca4b0ca76b49045bfc18190a934e528
Blake Loring <blake_l@parsed.uk>
2015-05-28 14:36:30 +0100

    Create jfreference.cpp

---
+++ src/jit/jfreference.cpp
@@ -0,0 +1,23 @@
+#include "jfreference.h"
+
+using namespace JIT;
+
+FunctionReference::FunctionReference() {
+  _fn = nullptr;
+}
+
+FunctionReference::FunctionReference(SafeFunction fn) {
+  set(fn);
+}
+
+SafeFunction FunctionReference::get() const {
+  return _fn;
+}
+
+void FunctionReference::set(SafeFunction fn) {
+  _fn = fn;
+}
+	
+SafeFunctionReference makeFunctionReference() {
+  return SafeFunctionReference(new FunctionReference());
+}
