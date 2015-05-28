57e587d68e82391b5a6cf018aa23a5a50a11aef5
Blake Loring <blake_l@parsed.uk>
2015-05-28 13:29:19 +0100

    Create jcallbacks.cpp

---
+++ src/jit/jcallbacks.cpp
@@ -0,0 +1,17 @@
+#include "jcallbacks.h"
+#include <stdio.h>
+
+using namespace JIT;
+
+int64_t Callbacks::print(Scope* scope, int64_t val) {
+  printf("Print Callback: %li\n", val);
+  return val;
+}
+
+int64_t Callbacks::set(Scope* scope, int64_t idx, int64_t val) {
+  scope->set(idx, val);
+}
+
+int64_t Callbacks::get(Scope* scope, int64_t idx) {
+  return scope->get(idx);
+}
