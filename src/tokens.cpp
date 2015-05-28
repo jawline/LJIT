46a98c2bee9c8ad37efc285d902647c06bd4fbda
Blake Loring <blake_l@parsed.uk>
2015-05-28 15:02:12 +0100

    Update tokens.cpp

--- src/tokens.cpp
+++ src/tokens.cpp
@@ -84,6 +84,9 @@ Token Tokeniser::peekToken(char const* input, size_t& len) {
 	} else if (strncmp(input, ")", 1) == 0) {
 		result = Token(RPAREN, input, 1);
 		len = 1;
+	} else if (strncmp(input, "->", 2) == 0) {
+		result = Token(ARROW, input, 2);
+		len = 2;
 	} else if (strncmp(input, "function", 8) == 0) {
 		result = Token(FUNCTION, input, 8);
 		len = 8;
