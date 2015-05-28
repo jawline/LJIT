cfdfe65a7d90a34e1873dbd8487dd60509e0f349
Blake Loring <blake_l@parsed.uk>
2015-05-28 13:35:52 +0100

    Update jstatement.cpp

--- src/jit/jstatement.cpp
+++ src/jit/jstatement.cpp
@@ -20,41 +20,41 @@ void Statement::write(Assembler::ByteBuffer& buffer) {
       Helper::pushNumber(_val, buffer);
       break;
     case Add:
-      _lhs->write(buffer);
-      _rhs->write(buffer);
+      args[0]->write(buffer);
+      args[1]->write(buffer);
       Helper::addTopTwoStack(buffer);
       break;
     case Subtract:
-      _lhs->write(buffer);
-      _rhs->write(buffer);
+      args[0]->write(buffer);
+      args[1]->write(buffer);
       Helper::subTopTwoStack(buffer);
       break;
     case Multiply:
-      _lhs->write(buffer);
-      _rhs->write(buffer);
+      args[0]->write(buffer);
+      args[1]->write(buffer);
       Helper::mulTopTwoStack(buffer);
       break;
     case Divide:
-      _lhs->write(buffer);
-      _rhs->write(buffer);
+      args[0]->write(buffer);
+      args[1]->write(buffer);
       Helper::divTopTwoStack(buffer);
       break;
     case Set:
-      _lhs->write(buffer);
-      _rhs->write(buffer);
+      args[0]->write(buffer);
+      args[1]->write(buffer);
       Helper::setArgumentZeroScope(buffer);
       Helper::setArgumentStackTop(2, buffer);
       Helper::setArgumentStackTop(1, buffer);
       Helper::callFunction((void*) Callbacks::set, buffer);
       break;
     case Get:
-      _lhs->write(buffer);
+      args[0]->write(buffer);
       Helper::setArgumentZeroScope(buffer);
       Helper::setArgumentStackTop(1, buffer);
       Helper::callFunction((void*) Callbacks::get, buffer);
       break;
     case Print:
-      _lhs->write(buffer);
+      args[0]->write(buffer);
       Helper::setArgumentZeroScope(buffer);
       Helper::setArgumentStackTop(1, buffer);
       Helper::callFunction((void*) Callbacks::print, buffer);
