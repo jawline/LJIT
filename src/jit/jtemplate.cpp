4845a2ecd66d051a5cbfcd94427ec06f5c014dcb
Blake Loring <blake_l@parsed.uk>
2015-05-28 09:23:51 +0100

    Update jtemplate.cpp

--- src/jit/jtemplate.cpp
+++ src/jit/jtemplate.cpp
@@ -4,12 +4,15 @@ using namespace JIT;
 
 uint8_t Template::prologue[] = {
     0x55, // push rbp (backup RBP)
+    0x41, 0x54, //push r12
+    0x49, 0x89, 0xFC, //mov rdi, r12
     0x48, 0x89, 0xE5 // mov rsp, rbp (backup RSP)
 };
 
 uint8_t Template::epilogue[] = {
     0x58, //pop rax (placing the result of our lovely math into RAX)
     0x48, 0x89, 0xEC, //mov rbp, rsp (restore RSP)
+    0x41, 0x5C, //pop r12
     0x5d, // pop rbp (restore RBP)
     0xC3 // ret
 };
