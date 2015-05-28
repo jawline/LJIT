786ff195606b031994694a2f92b503fe8997390f
Blake Loring <blake_l@parsed.uk>
2015-05-28 13:19:48 +0100

    Create jscope.h

---
+++ src/jit/jscope.h
@@ -0,0 +1,19 @@
+#ifndef _JIT_SCOPE_DEF_H_
+#define _JIT_SCOPE_DEF_H_
+#include <stdint.h>
+
+namespace JIT {
+  class Scope {
+  private:
+    int64_t* _data;
+
+  public:
+    Scope();
+    ~Scope();
+    
+    void set(int64_t idx, int64_t val);
+    void get(int64_t idx) const;
+  };
+}
+
+#endif
